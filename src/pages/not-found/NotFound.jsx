ad1bfbbc9446ba315542472f56c71bcea6f2e423
javasharipov <sharipovjavohir1227@gmail.com>
2025-01-27 18:57:19 +0500

    added some js

--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -2,16 +2,32 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const NotFound = () => {
-  return (
-    <div class="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-center">
-    <h1 class="text-9xl font-bold text-blue-600">404</h1>
-    <p class="text-2xl font-medium text-gray-800 mt-4">Sahifa topilmadi!</p>
-    <p class="text-gray-600 mt-2">Qidirayotgan sahifangiz mavjud emas yoki o'chirib tashlangan.</p>
-    <Link to={"/"} class="mt-6 inline-block px-6 py-3 bg-blue-600 text-white font-medium rounded-lg shadow-md hover:bg-blue-700 transition">
-      Bosh sahifaga qaytish
-    </Link>
-  </div>
-  )
+	return (
+		<div className='min-h-screen flex items-center justify-center bg-black text-white'>
+			<div className='text-center'>
+				<h1 className='text-9xl font-extrabold text-red-500 mb-6'>404</h1>
+				<p className='text-2xl font-semibold mb-4'>
+					Oops! The page you’re looking for doesn’t exist.
+				</p>
+				<p className='mb-8 text-lg'>
+					But hey, don't panic, maybe you typed something wrong.
+				</p>
+				<button
+					onClick={() => (window.location.href = '/')}
+					className='bg-red-600 text-white py-3 px-6 rounded-lg hover:bg-red-500 transition duration-300'
+				>
+					Go Back Home
+				</button>
+				<div className='mt-10'>
+					<img
+						src='https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif'
+						alt='Confused Travolta'
+						className='w-80 mx-auto rounded-xl shadow-lg'
+					/>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default NotFound
